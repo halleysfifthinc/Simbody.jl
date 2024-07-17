9b7d6f126394913469b5c85c44baf285c4992b2b
Allen Hill <allenofthehills@gmail.com>
2024-07-17 15:17:26 -0700

    Update to match jlsimbody

--- src/Simbody.jl
+++ src/Simbody.jl
@@ -10,9 +10,20 @@ function __init__()
     @initcxx
 end
 
-export SimTKArray, SimTKVec, SimTKRow, SimTKMat, SimTKUnitVec, Rotation, InverseRotation,
-    Transform, InverseTransform
+# Types
+export SimTKArray, SimTKVec, SimTKRow, SimTKMat, SimTKUnitVec, Quaternion, Rotation,
+    InverseRotation, Transform, InverseTransform, Inertia, UnitInertia, SpatialInertia,
+    ArticulatedInertia, MassProperties, DecorativeGeometryRepresentation, PolygonalMesh,
+    DecorativeGeometry, DecorativePoint, DecorativeLine, DecorativeCircle, DecorativeSphere,
+    DecorativeEllipsoid, DecorativeBrick, DecorativeCylinder, DecorativeFrame,
+    DecorativeText, DecorativeMesh, DecorativeMeshFile, DecorativeTorus, DecorativeArrow,
+    DecorativeCone, Decorations, Stage, StageLevel, EventCauseNum, EventTrigger,
+    HandleEventsOption, HandleEventsResult, Event, EventCause, EventTriggerInfo,
+    HandleEventsOptions, HandleEventsResults, State, AbstractValue, EventHandler,
+    ScheduledEventHandler, TriggeredEventHandler, PeriodicEventHandler, EventReporter,
+    ScheduledEventReporter, TriggeredEventReporter, PeriodicEventReporter
 
+# Type-aliases
 export Vec3, Row3, Mat33, UnitVec, SpatialVec, SpatialMat
 
 # export cxxparametricsubtypes
@@ -22,5 +33,6 @@ include("simtk_arrays.jl")
 include("orientation_transformation.jl")
 include("massproperties.jl")
 include("decorations.jl")
+include("state.jl")
 
 end
---
+++ src/state.jl
@@ -0,0 +1,18 @@
+# Simbody doesn't define a copy-constructor
+HandleEventsOptions(o::reference_type_union(HandleEventsOptions)) = bwor!(HandleEventsOptions(getAccuracy(a)), a)
+
+# Separate def because HandleEventsOptions doesn't define
+# operator&=(HandleEventsOptions, HandleEventsOption)
+function Base.:(&)(a::HandleEventsOptions, b::reference_type_union(HandleEventsOptions))
+    c = HandleEventsOptions(a)
+    bwand!(c, b)
+    return c
+end
+
+for (op, func) in zip((:|, :-), (bwor!, sub!))
+@eval function Base.$(op)(a::HandleEventsOptions, b::Union{reference_type_union(HandleEventsOptions), HandleEventsOption})
+          c = HandleEventsOptions(a)
+          ($func)(c, b)
+          return c
+      end
+end
