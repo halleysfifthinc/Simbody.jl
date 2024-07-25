e5f06bfaf3fd4b198f5381dbf88c8b762834d340
Allen Hill <allenofthehills@gmail.com>
2024-07-24 17:59:40 -0700

    Fix non include of utils

--- src/Simbody.jl
+++ src/Simbody.jl
@@ -81,7 +81,7 @@ export Vec3, Row3, Mat33, UnitVec, SpatialVec, SpatialMat, TorsionMobilizedBody,
 
 # export cxxparametricsubtypes
 
-# include("utils.jl")
+include("utils.jl")
 include("simtk_arrays.jl")
 include("orientation_transformation.jl")
 include("massproperties.jl")
